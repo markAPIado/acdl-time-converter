973f92aa08b5a10a7130911aa16039cd8ec03672
Mark Apiado <apiadomptc@gmail.com>
2023-11-11 12:52:02 +0800

    refactor: implement time components, add location and are

--- src/public/components/CurrentTime.tsx
+++ src/public/components/CurrentTime.tsx
@@ -1,4 +1,6 @@
 import useWorldTimeApi from '../hooks/useWorldTimeApi';
+import MeridiemIndicator from './MeridiemIndicator';
+import TimeDisplay from './TimeDisplay';
 interface CurrentTimeProps {
   timeZone: string;
 }
@@ -13,43 +15,22 @@ export default function CurrentTime({ timeZone }: CurrentTimeProps) {
       ? currentTime.hours - 12
       : currentTime.hours;
 
+  const location =
+    timeZone.split('/')[1]?.split('_').join(' ') ?? timeZone.split('/')[0];
+
+  const area = timeZone.split('/')[0];
+
   return (
     <>
       <div className="mr-20 flex flex-col">
-        <span className="countdown font-mono text-5xl">
-          {timeZone.split('/')[1]?.split('_').join(' ') ??
-            timeZone.split('/')[0]}
-        </span>
-        {timeZone.split('/')[0]}
+        <span className="countdown font-mono text-5xl">{location}</span>
+        {area}
       </div>
       <div className="grid auto-cols-max grid-flow-col gap-5 text-center">
-        <div className="flex flex-col">
-          <span className="countdown font-mono text-5xl">
-            <span style={{ '--value': hours } as React.CSSProperties}></span>
-          </span>
-          hours
-        </div>
-        <div className="flex flex-col">
-          <span className="countdown font-mono text-5xl">
-            <span
-              style={{ '--value': currentTime.minutes } as React.CSSProperties}
-            ></span>
-          </span>
-          min
-        </div>
-        <div className="flex flex-col">
-          <span className="countdown font-mono text-5xl">
-            <span
-              style={{ '--value': currentTime.seconds } as React.CSSProperties}
-            ></span>
-          </span>
-          sec
-        </div>
-        <div className="flex flex-col">
-          <span className="countdown font-mono text-5xl">
-            {currentTime.meridiemIndicator}
-          </span>
-        </div>
+        <TimeDisplay time={hours} text="hours" />
+        <TimeDisplay time={currentTime.minutes} text="min" />
+        <TimeDisplay time={currentTime.seconds} text="sec" />
+        <MeridiemIndicator text={currentTime.meridiemIndicator} />
       </div>
     </>
   );
